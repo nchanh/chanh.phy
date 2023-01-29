acd84433c5c0552f3b95bfd9f67c45239dd1facb
Nguyen Chanh <nguyenchanh0306@gmail.com>
2023-01-29 17:46:28 +0700

    chore: service.js

--- service.js
+++ service.js
@@ -1,64 +1,115 @@
 const IMAGES = [
-  {
-    id: 1,
-    name: "m1",
-    src: "/assets/m1.jpg",
-  },
-  {
-    id: 2,
-    name: "m2",
-    src: "/assets/m2.jpg",
-  },
-  {
-    id: 3,
-    name: "m3",
-    src: "/assets/m3.jpg",
-  },
-  {
-    id: 4,
-    name: "a1",
-    src: "/assets/a1.jpg",
-  },
-  {
-    id: 5,
-    name: "a2",
-    src: "/assets/a2.jpg",
-  },
-  {
-    id: 6,
-    name: "a3",
-    src: "/assets/a3.jpg",
-  },
-  {
-    id: 7,
-    name: "s1",
-    src: "/assets/s1.jpg",
-  },
-  {
-    id: 8,
-    name: "s2",
-    src: "/assets/s2.jpg",
-  },
-  {
-    id: 9,
-    name: "s3",
-    src: "/assets/s3.jpg",
-  },
+  [
+    {
+      id: 1,
+      caption: "m1",
+      media_url: "/assets/m1.jpg",
+    },
+    {
+      id: 2,
+      caption: "m2",
+      media_url: "/assets/m2.jpg",
+    },
+    {
+      id: 3,
+      caption: "m3",
+      media_url: "/assets/m3.jpg",
+    },
+    {
+      id: 4,
+      caption: "a1",
+      media_url: "/assets/a1.jpg",
+    },
+    {
+      id: 5,
+      caption: "a2",
+      media_url: "/assets/a2.jpg",
+    },
+    {
+      id: 6,
+      caption: "a3",
+      media_url: "/assets/a3.jpg",
+    },
+    {
+      id: 7,
+      caption: "s1",
+      media_url: "/assets/s1.jpg",
+    },
+    {
+      id: 8,
+      caption: "s2",
+      media_url: "/assets/s2.jpg",
+    },
+    {
+      id: 9,
+      caption: "s3",
+      media_url: "/assets/s3.jpg",
+    },
+  ]
 ];
 
-const newBlock = document.createDocumentFragment();
-IMAGES.forEach((item) => {
-  const newDiv = document.createElement("div");
-  newDiv.id = `item-${item.id}`;
-  newDiv.className = "item col-lg-4";
+const isValid = (_posts, _idx) => _posts.length - 1 <= _idx;
 
-  const pathName = window.location.href
-  const newImg = document.createElement("img");
-  newImg.alt = item.name;
-  newImg.src = pathName + item.src;
-  newDiv.appendChild(newImg);
+const hdlStatusBtnPaginate = (_posts, _idx) => {
+  const elmBtnPaginate = document.getElementById("buttonPaginate");
+  if (isValid(_posts, _idx)) {
+    elmBtnPaginate.style.display = "none";
+  } else {
+    elmBtnPaginate.style.display = "block";
+  }
+};
 
-  newBlock.appendChild(newDiv);
-});
+const printPhotos = (_posts, _idx) => {
+  const eleWrapper = document.createDocumentFragment();
 
-document.getElementById("wrapper").appendChild(newBlock);
+  // caption, id, media_type, media_url, permalink, timestamp, is_shared_to_feed
+  _posts[_idx].forEach((item) => {
+    const eleDivItem = document.createElement("div");
+    eleDivItem.id = `item-${item.id}`;
+    eleDivItem.className = "item col-lg-4";
+
+    // Begin - Children eleDivItem
+    const eleImage = document.createElement("img");
+    eleImage.alt = item.timestamp;
+    eleImage.src = item.media_url;
+    eleDivItem.appendChild(eleImage);
+
+    const eleInfo = document.createElement("div");
+    eleInfo.className = "image-info";
+
+    // Begin - Children eleInfo
+    const eleInfoText = document.createElement("pre");
+    eleInfoText.className = "text";
+    eleInfoText.innerHTML = item.caption;
+    eleInfo.appendChild(eleInfoText);
+
+    const eleInfoLink = document.createElement("a");
+    eleInfoLink.className = "btn-link";
+    eleInfoLink.href = item.permalink;
+    eleInfoLink.target = "_blank";
+    eleInfoLink.innerHTML = "See details";
+    eleInfo.appendChild(eleInfoLink);
+    // End - Children eleInfo
+
+    eleDivItem.appendChild(eleInfo);
+
+    const eleOverlay = document.createElement("div");
+    eleOverlay.className = "overlay";
+    eleDivItem.appendChild(eleOverlay);
+    // End - Children eleInfo
+
+    eleWrapper.appendChild(eleDivItem);
+  });
+
+  document.getElementById("wrapper").appendChild(eleWrapper);
+  hdlStatusBtnPaginate(_posts, _idx);
+};
+
+let idx = 0;
+const posts = IMAGES;
+printPhotos(posts, idx);
+
+const hdlClickPaginate = () => {
+  idx++;
+  printPhotos(posts, idx);
+};
